fc50051ccd9ea8b60fb1f5041d9420f40f4173a6
emmabh <emmabh97@bu.edu>
2020-01-30 20:39:31 -0500

    added procfile

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const config = require('./config.json');
-var uri = 'mongodb+srv://' + config.db.username + ':' + config.db.pw + '@secretkeeper-ytgv2.mongodb.net/secretKeeper';
+var uri = 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PW + '@secretkeeper-ytgv2.mongodb.net/secretKeeper';
 
 
 class Database {
